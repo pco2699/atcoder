7ed212d9fc32c368533f72fe29b8d165b4651d7f
pco2699 <aenkun@gmail.com>
2019-10-20 12:38:47 +0900

    fix: ABC143/E

--- ABC143/E.py
+++ ABC143/E.py
@@ -17,26 +17,7 @@ def e():
         li_st.append([s, t])
 
     for k in range(n):
-        for i in range(n):from bisect import bisect_right
-
-
-def d():
-    n = int(input())
-    ls = list(map(int, input().split()))
-    ls.sort()
-    count = 0
-    for i in range(n-1, -1, -1):
-        for j in range(i-1, -1, -1):
-            k = bisect_right(ls, ls[i]-ls[j])
-            if j >= k:
-                count += j - k
-
-    print(count)
-
-
-if __name__ == '__main__':
-    d()
-
+        for i in range(n):
             for j in range(n):
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
