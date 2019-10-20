71bc92314abdc22412133936620ea989e9e15b88
pco2699 <aenkun@gmail.com>
2019-10-20 12:36:56 +0900

    fix: delete .idea

--- ABC143/E.py
+++ ABC143/E.py
@@ -17,7 +17,26 @@ def e():
         li_st.append([s, t])
 
     for k in range(n):
-        for i in range(n):
+        for i in range(n):from bisect import bisect_right
+
+
+def d():
+    n = int(input())
+    ls = list(map(int, input().split()))
+    ls.sort()
+    count = 0
+    for i in range(n-1, -1, -1):
+        for j in range(i-1, -1, -1):
+            k = bisect_right(ls, ls[i]-ls[j])
+            if j >= k:
+                count += j - k
+
+    print(count)
+
+
+if __name__ == '__main__':
+    d()
+
             for j in range(n):
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
